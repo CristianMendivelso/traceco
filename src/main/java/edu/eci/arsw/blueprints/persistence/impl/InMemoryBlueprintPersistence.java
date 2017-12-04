e7da2efe78e7cdd83493ea8da4d4579bd49f790f
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-12-04 15:34:38 -0500

    nuevos datos

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -36,24 +36,39 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence {
         //load stub data
         ArrayList<Tour> tourtemp= new ArrayList<Tour>();
         ArrayList<Fecha> fechatemp= new ArrayList<Fecha>();
-        HashMap<Integer,Long> preciotemp= new HashMap<Integer,Long> ();
-        Tour t= new Tour(0, preciotemp, "Alumbrado Nocturno Cali", "Disfruta del recorrido por el alumbrado navideño", fechatemp, "sergioperez", "https://i.ytimg.com/vi/NtCOOhxS1s8/maxresdefault.jpg", 60);
-        tourtemp.add(t);
         Fecha f= new Fecha(0, 0, "23-12-2017 18:00");
         Fecha f1= new Fecha(1, 0, "22-12-2017 18:00");
         Fecha f2= new Fecha(2, 0, "21-12-2017 18:00");
         fechatemp.add(f);
         fechatemp.add(f1);
         fechatemp.add(f2);
-        preciotemp.put(1,Long.parseLong("20000"));
-        preciotemp.put(2,Long.parseLong("35000"));
-        preciotemp.put(3,Long.parseLong("20000"));
-        Usuario u= new Usuario("cristiansoto", "Christian Soto", "password","csoto@mail.com", "viajero", Long.parseLong("320323342"),tourtemp, "https://www.definicionabc.com/wp-content/uploads/social/Viajero-2-turismo.jpg");
-        Usuario u1= new Usuario("sergioperez", "Sergio Perez", "password","sgperez@mail.com", "guia", Long.parseLong("3145346578"),tourtemp, "http://www.cechotec.cl/web/media/producto/p/59/59-producto_large_92fb5485d7.jpg");
+        HashMap<Integer,Long> preciotemp= new HashMap<Integer,Long> ();
+        Tour t= new Tour(0, preciotemp, "Alumbrado Nocturno Cali", "Disfruta del recorrido por el alumbrado navideño", fechatemp, "carlosr", "https://i.ytimg.com/vi/NtCOOhxS1s8/maxresdefault.jpg", 60);
+        Tour t2= new Tour(0, preciotemp, "Recorrido Diurno Cali", "Viaje En Coche Por la Ciudad", fechatemp, "carlosr", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Augusto_Ilian_Rio_Cali_Cali.jpg/210px-Augusto_Ilian_Rio_Cali_Cali.jpg", 60);
+        tourtemp.add(t);
+        tourtemp.add(t2);
+        ArrayList<Tour> tourtemp2= new ArrayList<Tour>();
+        ArrayList<Tour> tourtemp3= new ArrayList<Tour>();
 
+        HashMap<Integer,Long> preciotemp2= new HashMap<Integer,Long> ();
+
+        preciotemp2.put(1,Long.parseLong("50000"));
+        preciotemp2.put(2,Long.parseLong("90000"));
+        preciotemp2.put(3,Long.parseLong("135000"));
+        Tour t3= new Tour(0, preciotemp2, "Cerrro de Monserrate Bogota", "Incluye Subida a telesférico", fechatemp, "sergioperez", "http://du4zwgdg3nwxa.cloudfront.net/empresas/imagenes/a/au/auzlibvr-1354910060-bg.jpg", 60);
+        Tour t4= new Tour(0, preciotemp2, "Torre Colpatria Bogota", "Incluye Subida a telesférico", fechatemp, "sergioperez", "https://assets.metrolatam.com/co/2016/11/30/torre-colpatria-1200x600.jpg", 60);
+        tourtemp3.add(t3);
+        tourtemp3.add(t4);
+        Usuario u= new Usuario("csoto", "Christian Soto", "password","csoto@mail.com", "viajero", Long.parseLong("320323342"),tourtemp2, "https://www.definicionabc.com/wp-content/uploads/social/Viajero-2-turismo.jpg");
+        Usuario u1= new Usuario("carlosr", "Carlos Ramirez", "password","carlosr@mail.com", "guia", Long.parseLong("3145346578"),tourtemp, "http://www.cechotec.cl/web/media/producto/p/59/59-producto_large_92fb5485d7.jpg");
+        Usuario u2= new Usuario("sergioperez", "Sergio Perez", "password","sergioperez@mail.com", "guia", Long.parseLong("3145346578"),tourtemp3, "http://hotelcenter.com.br/blog/wp-content/uploads/2016/07/4-dicas-para-se-tornar-um-bom-guia-turistico-1000x563.jpg");
         usuarios.add(u);
-         usuarios.add(u1);
+        usuarios.add(u1);
+        usuarios.add(u2);
         tours.add(t);
+        tours.add(t2);
+        tours.add(t3);
+        tours.add(t4);
         
     }
 
